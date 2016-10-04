187c7766a69dfe61b69098e8dea38b56fbf0ff93
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-10-04 13:58:49 +0200

    stubs for php

---
+++ php/src/BirthdayService.php
@@ -0,0 +1,23 @@
+<?php
+
+class BirthdayService {
+    
+    var $clientRepository;
+    
+    public function __construct($clientRepository) {
+        $this->clientRepository = $clientRepository;
+    }
+    
+    public function greetingFor($name) {
+        $greeting;
+        if ($this->clientRepository->birthDayIsTodayFor($name)) {
+            $greeting = "Happy birthday $name!";
+        }
+        else {
+            $greeting = "Good morning $name.";
+        }
+    }
+    
+}
+
+?>
