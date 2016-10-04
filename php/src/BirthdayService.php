4fe5968d2374ea8836b393820540f120bf5b5f39
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-10-04 23:07:55 +0200

    return value from greeting()

--- php/src/BirthdayService.php
+++ php/src/BirthdayService.php
@@ -9,13 +9,14 @@ class BirthdayService {
     }
     
     public function greetingFor($name) {
-        $greeting;
+        $greeting = "";
         if ($this->clientRepository->birthDayIsTodayFor($name)) {
             $greeting = "Happy birthday $name!";
         }
         else {
             $greeting = "Good morning $name.";
         }
+        return $greeting;
     }
     
 }
