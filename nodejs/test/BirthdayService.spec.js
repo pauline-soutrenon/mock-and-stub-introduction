13041b3e176118c25bd00f80590f18e60d9107e9
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-02-01 11:39:35 +0100

    add strict

--- nodejs/test/BirthdayService.spec.js
+++ nodejs/test/BirthdayService.spec.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var expect = require('chai').expect;
+let sinon = require('sinon')
 
 describe('BirthdayService', function() {
     var BirthdayService = require('../src/BirthdayService');
