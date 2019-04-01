7bda849daade476e25fb388c3ec1d86441e04f33
johan martinsson <jm1974@hotmail.com>
2019-04-02 00:12:43 +0200

    php

--- php/stubSomeValues/test/BirthdayServiceTest.php
+++ php/stubSomeValues/test/BirthdayServiceTest.php
@@ -2,7 +2,43 @@
 
 namespace stubSomeValues;
 
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophet;
+
 class BirthdayServiceTest extends TestCase
 {
+    /** @test */
+    public function wishesHappyBirthdayOnTheDay()
+    {
+        $prophecy = $this->newProphecy();
+        $prophecy->birthDayIsTodayFor("Arnaud")->willReturn(true);
+        $birthdayService = new BirthdayService($prophecy->reveal());
+
+        $this->assertEquals("Happy birthday Arnaud!", $birthdayService->greetingFor("Arnaud"));
+    }
+
+    /** @test */
+    public function simplySaysGoodMorningOnOtherDays()
+    {
+        $prophecy = $this->newProphecy();
+        $prophecy->birthDayIsTodayFor("Fred")->willReturn(false);
+        $birthdayServier = new BirthdayService($prophecy->reveal());
+
+        $greeting = $birthdayServier->greetingFor("Fred");
+
+        $this->assertEquals("Good morning Fred.", $greeting);
+
+    }
+
+
+    /**
+     * @return \Prophecy\Prophecy\ObjectProphecy
+     */
+    private function newProphecy(): \Prophecy\Prophecy\ObjectProphecy
+    {
+        $prophecy = (new Prophet())->prophesize(ClientRepository::class);
+        return $prophecy;
+    }
+
 
 }
--- php/verifySomeBehavior/test/BirthdayServiceTest.php
+++ php/verifySomeBehavior/test/BirthdayServiceTest.php
@@ -3,14 +3,40 @@
 namespace verifySomeBehavior;
 
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophet;
 
 class BirthdayServiceTest extends TestCase
 {
+
     /** @test */
-    public function Name()
+    public function wishesHappyBirthdayOnTheDay()
     {
 
+        $mailerProphecy = $this->prophesize(Mailer::class);
+        $repositoryProphecy = $this->prophesize(ClientRepository::class);
+
+        $service = new BirthdayService($repositoryProphecy->reveal(), $mailerProphecy->reveal());
+
+        $service->greetingFor("Cloe");
+
+        $mailerProphecy->send('Good morning Cloe.')->shouldBeCalled();
     }
+    
+    /** @test */
+    public function simplySaysGoodMorningOnOtherDays()
+    {
+
+        $mailerProphecy = $this->prophesize(Mailer::class);
+        $repositoryProphecy = $this->prophesize(ClientRepository::class);
+        $repositoryProphecy->birthDayIsTodayFor("Cloe")->willReturn(true);
+
+        $service = new BirthdayService($repositoryProphecy->reveal(), $mailerProphecy->reveal());
+
+        $service->greetingFor("Cloe");
+
+        $mailerProphecy->send('Happy birthday Cloe!')->shouldBeCalled();
+    }
+
 
 
 }
