7fdd203b4e239a0eb33614dfcb1a55680c2d5b4e
Pauline <psoutrenon@comongo.fr>
2019-07-18 16:14:54 +0200

    working on stub, spy

--- nodejs/stubSomeValues/test/BirthdayService.spec.js
+++ nodejs/stubSomeValues/test/BirthdayService.spec.js
@@ -5,11 +5,29 @@ let sinon = require('sinon')
 
 describe('BirthdayService', function() {
     var BirthdayService = require('../src/BirthdayService');
-
+    var clientRepository;
+    let b;
     describe('greeting()', function() {
-        it('does something', function() {
-
-
+        beforeEach(function () {
+            clientRepository = {
+                birthdayIsTodayFor: sinon.stub()
+            };
+            b = new BirthdayService(clientRepository);
+        });
+        it('false', function() {
+            clientRepository.birthdayIsTodayFor.withArgs('fred').returns(false)
+            const s = b.greeting('fred')
+            expect(s).equal('Good morning fred.')
+        });
+        it('is fred birthday', function() {
+            clientRepository.birthdayIsTodayFor.withArgs('fred').returns(true)
+            const s = b.greeting('fred')
+            expect(s).equal('Happy birthday fred!')
+        });
+        it('is not sandra birthday', function() {
+            clientRepository.birthdayIsTodayFor.withArgs('fred').returns(true)
+            const s = b.greeting('sandra')
+            expect(s).equal('Good morning sandra.')
         });
     });
 });
--- nodejs/verifySomeBehavior/test/BirthdayService.spec.js
+++ nodejs/verifySomeBehavior/test/BirthdayService.spec.js
@@ -3,14 +3,77 @@
 var expect = require('chai').expect;
 let sinon = require('sinon')
 
-describe('BirthdayService', function() {
+describe('BirthdayService', function () {
     var BirthdayService = require('../src/BirthdayService');
 
-    describe('greeting()', function() {
-        it('does something', function() {
+    describe('greeting()', function () {
+        it('good morning', function () {
+            // Given
+            const repository = {
+                birthdayIsTodayFor: sinon.stub()
+            }
+            const mailer = {
+                send: sinon.spy()
+            }
+            const b = new BirthdayService(repository, mailer)
 
-//            use sinon.assert.calledWith(spy, "...");
+            // When
+            b.greeting('fred')
 
+            // Then
+            sinon.assert.calledWith(mailer.send, "Good morning fred.");
+        });
+        it('happy birthday', function () {
+            // Given
+            const repository = {
+                birthdayIsTodayFor: sinon.stub()
+            }
+            repository.birthdayIsTodayFor.returns(true)
+            const mailer = {
+                send: sinon.spy()
+            }
+            const b = new BirthdayService(repository, mailer)
+
+            // When
+            b.greeting('fred')
+
+            // Then
+            sinon.assert.calledWith(mailer.send, "Happy birthday fred!");
+        });
+        it('happy birthday with matchers', function () {
+            // Given
+            const repository = {
+                birthdayIsTodayFor: sinon.stub()
+            }
+            repository.birthdayIsTodayFor.returns(true)
+            const mailer = {
+                send: sinon.spy()
+            }
+            const b = new BirthdayService(repository, mailer)
+
+            // When
+            b.greeting('fred')
+
+            // Then
+            sinon.assert.calledWith(mailer.send, sinon.match(/Happy.*?/));
+        });
+        it('happy birthday', function () {
+            // Given
+            const repository = {
+                birthdayIsTodayFor: sinon.stub()
+            }
+            repository.birthdayIsTodayFor.returns(true)
+            const mailer = {
+                send: sinon.spy()
+            }
+            const b = new BirthdayService(repository, mailer)
+
+            // When
+            b.greeting('fred')
+
+            // Then
+            let greeting = mailer.send.firstCall.args[0]
+            expect(greeting).include("Happy birthday")
         });
     });
 });
